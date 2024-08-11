e9bc0da0e67fb535d5bae389f073aa629ee9b665
Sina Baradaran <sinabaradaran98@gmail.com>
2024-08-12 00:42:29 +0330

    feat: add icon loader base on dynamic import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import IconLoader from './components/shared/IconLoader.vue';
+</script>
 
 <template>
   <div>
+    <IconLoader icon="account" color="#fff" width="20" height="20" />
     <h1>سلام</h1>
   </div>
 </template>
---
+++ src/components/icons/IconArrowLeft.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path
+      d="M9.57 5.92969L3.5 11.9997L9.57 18.0697"
+      stroke="#292D32"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M20.5 12H3.66998"
+      stroke="#292D32"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
