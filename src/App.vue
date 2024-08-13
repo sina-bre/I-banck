ba0898dffa5b3a41c26a41588c90be99544c429c
Yasamin Poorhashemi <yasamin.po.1999@gmail.com>
2024-08-13 12:56:48 +0330

    feat:cards dropdown box created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import DropdownBox from './components/global/DropdownBox.vue';
+import IconLoader from './components/shared/IconLoader.vue';
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <DropdownBox>
+      <template v-slot:first-icon>
+        <IconLoader icon="account" width="1.5rem" height="1.5rem" color="black" />
+      </template>
+      <template v-slot:second-icon>
+        <IconLoader icon="logo" width="1.5rem" height="1.5rem" color="red" />
+      </template>
+    </DropdownBox>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
