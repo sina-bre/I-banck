82dc673ace7f460c7b82328625c119119f3d7bb6
Sina Baradaran <sinabaradaran98@gmail.com>
2024-08-14 19:30:40 +0330

    fix:fix conflicts in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@ import TheSidebar from './components/global/TheSidebar.vue';
 </script>
 
 <template>
-<<<<<<< Updated upstream
-  <RouterView></RouterView>
-=======
   <RouterView />
-  <!-- <TheSidebar /> -->
->>>>>>> Stashed changes
 </template>
 
 <style scoped lang="scss"></style>
